1e19b868aec2badd34c7cb46ca5f8e4888393e9d
张渝宁 <neuromancer43@gmail.com>
2017-05-28 10:41:16 +0800

    nearly finished

--- Shenzheng_Cup/Markov_Model.py
+++ Shenzheng_Cup/Markov_Model.py
@@ -39,8 +39,8 @@ def G(p):
     return np.sum([np.power(2, i - 1) * np.power(p, i) for i in range(6)])
 
 
-class StateMahcine(object):
-    def __init__(self, W_0, C_0, L_0=0, E_0=1, R_0):
+class TCPState(object):
+    def __init__(self, W_0, C_0, L_0=0, E_0=1, R_0=0, Wmax,RTT,T0):
         '''
         W_0: transmition windowa
         C_0:
@@ -49,5 +49,35 @@ class StateMahcine(object):
         R_: bool, if retransmition
         '''
         self.state = tuple(W_0, C_0, L_0, E_0, R_0)
-    def update():
+        self.Wmax = Wmax
+        self.RTT=RTT
+        self.T0=T0
+    def update(p):
+        '''
+        p: the chance of package loss
+        '''
+        w,c,l,e,r=self.state
+        loss=0
+        for i in range(W_0):
+            if random.random()<p:
+                loss+=1
+            
+        if loss=0:
+            if 2*w<self.Wmax:
+                w*=2 # slow start
+            else:
+                if c=1:
+                    w+=1 # congestion avoidance
+                    c=0
+                else:
+                    c=1
+        elif 0<loss and loss<w:
+            w = w-loss
+            l=loss
+            c=0
+            e=1
+            
+            
+                        
+
         
